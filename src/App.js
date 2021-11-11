e9ef0d20c1f29aca569347bc8926cf0f1a3eec1a
Cyclip <gdneop@gmail.com>
2021-11-11 20:25:19 +0000

    Add side bar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,68 @@
 import React from 'react'
-import logo from './logo.svg'
-import tauriCircles from './tauri.svg'
-import tauriWord from './wordmark.svg'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="inline-logo">
-          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
-          <img src={tauriWord} className="App-logo smaller" alt="logo" />
-        </div>
-        <a
-          className="App-link"
-          href="https://tauri.studio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tauri
-        </a>
-        <img src={logo} className="App-logo rotate" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabTop: 15,
+    };
+  }
+
+  changeTab(event, arg) {
+    switch(arg) {
+      case "embed":
+        this.setState({tabTop: 15});
+        break;
+      case "extract":
+        this.setState({tabTop: 15 + 70});
+        break;
+      case "passwords":
+        this.setState({tabTop: 15 + 140});
+        break;
+      case "help":
+        this.setState({tabTop: 15 + 210});
+        break;
+    }
+    
+    console.log("changed tab", arg, this.state.tabTop);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="frames">
+            <div className="nav noselect">
+              <h2>ZeroDrop</h2>
+              <div className="items">
+                <h3 onClick={(e) => {
+                  this.changeTab(e, 'embed')
+                }}>embed</h3>
+  
+                <h3 onClick={(e) => {
+                  this.changeTab(e, 'extract')
+                }}>extract</h3>
+  
+                <h3 onClick={(e) => {
+                  this.changeTab(e, 'passwords')
+                }}>passwords</h3>
+  
+                <h3 onClick={(e) => {
+                  this.changeTab(e, 'help')
+                }}>help</h3>
+  
+                <div 
+                  id="selector" 
+                  className="arrow-left tabSelector"
+                  style={{top: this.state.tabTop}}></div>
+              </div>
+            </div>
+          </div> 
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App
